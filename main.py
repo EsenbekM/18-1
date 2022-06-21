a28840cdae3fa811f91b33a0d5c6aadca634e5dd
Esen <esenbekm03@gmail.com>
2022-06-21 19:40:51 +0600

    lesson 5

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import asyncio
+
 from aiogram.utils import executor
 
 from config import dp
-from handlers import extra, client, admin, callback, fsm_anketa
+from handlers import extra, client, admin, callback, fsm_anketa, notification
 import logging
 from database.bot_db import sql_create
 
 
 async def on_startup(_):
+    asyncio.create_task(notification.scheduler())
     sql_create()
 
 
@@ -14,6 +17,7 @@ client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 fsm_anketa.register_handler_fsmanketa(dp)
+notification.register_handler_notification(dp)
 
 extra.register_handlers_extra(dp)
 
--- test.py
+++ test.py
@@ -1,4 +1,39 @@
-from decouple import config
+"""
+Задача №1.
+Есть у нас список учеников которые сдавали тест
+Нужно вывести средний балл всех стужентов ,
+этот результат надо сравнить со своим баллом
+Если ваш балл ниже среднего значит надо вернуть "Провал"
+Если ваше балл выше надо вернуть "Прошел"
+"""
 
-passw = config("PASSWORD", default=123, cast=int)
-print(passw)
+# class_points = [12, 53, 86, 90, 32, 64]
+# my_point = 12
+# print((sum(class_points) / len(class_points)))
+#
+#
+# def average(class_points, my_point):
+#     return "Passed" if (sum(class_points) / len(class_points)) < my_point else "Fail"
+#     # if (sum(class_points) / len(class_points)) < my_point:
+#     #     return "Passed"
+#     # return "Fail"
+#
+#
+# print(average(class_points, my_point))
+
+"""
+Вы решили отправиться на отдых 
+Там понадобилось вам арендовать машину
+Арендатель говорит , аренда авто в день стоит 40$
+Если вы будете арендовать больше или равно 7 дней,
+ дает скидку 50$
+Если вы арендуете меньше 7 дней но больше 3 дней , 
+он все равно дает скидку 20$
+"""
+
+
+def arenda(days):
+    return (days * 40) if days <= 3 else (days * 40) - 20 if 3 < days < 7 else (days * 40) - 50
+
+
+print(arenda(2))
